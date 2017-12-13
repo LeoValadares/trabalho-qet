516dfde887968b8b1515f8d1b55de43ff6c4a5bd
Leonardo Valadares <leonardo@Leo-MBP.home>
2017-12-13 02:34:19 -0200

    testes com jest

---
+++ SearchFunctions.js
@@ -0,0 +1,66 @@
+const ResultType = require('./ResultType');
+const SearchResult = require('./SearchResultEnum');
+const SearchType = require('./SearchType');
+
+function equals (item, key) {
+    return item === key ? SearchResult.FoundExact : SearchResult.NotFound;
+}
+
+function lessThan (item, key) {
+    return item < key ? SearchResult.FoundLess : SearchResult.NotFound;
+}
+
+function greaterThan (item, key) {
+    return item > key ? SearchResult.FoundGreater : SearchResult.NotFound;
+}
+
+function arrayTraverser (key, items, size, reverseTraverse, searchFunction) {
+	let result = new ResultType();
+	let currPos = reverseTraverse ? size - 1 : 0;
+	let finishingPos = reverseTraverse ? 0 : size - 1;
+	let traverseStep = reverseTraverse ? (i) => --i : (i) => ++i;
+
+	while(currPos != finishingPos) {
+		let searchFunctionResult = searchFunction(items[currPos], key);
+		if (searchFunctionResult !== SearchResult.NotFound) {
+			result.searchResult = searchFunctionResult;
+			result.resultIndex = currPos;
+			result.resultValue = items[currPos];
+			return result;
+		}
+		currPos = traverseStep(currPos);
+	}
+    return result;
+}
+
+function searchArrays (key, items, size, ascending, type) {
+    //evitando lógica complexa de guardar estado de iteração anterior para descobrir o maior menor
+    //e menor maior fazendo hack entre ascending, tipo de busca e sentido de iteração do array
+    if (type === SearchType.LessThan) {
+        return arrayTraverser(key, items, size, ascending, lessThan);
+    }
+    if (type === SearchType.LessThanEquals) {
+        let equalsResult = arrayTraverser(key, items, size, !ascending, equals);
+        if (equalsResult.searchResult === SearchResult.NotFound)
+            return arrayTraverser(key, items, size, ascending, lessThan);
+        return equalsResult;
+    }
+    if (type === SearchType.Equals) {
+        return arrayTraverser(key, items, size, !ascending, equals);
+    }
+    if (type === SearchType.GreaterThan) {
+        return arrayTraverser(key, items, size, !ascending, greaterThan);
+    }
+    if (type === SearchType.GreaterThanEquals) {
+        let equalsResult = arrayTraverser(key, items, size, !ascending, equals);
+        if (equalsResult.searchResult === SearchResult.NotFound)
+            return arrayTraverser(key, items, size, !ascending, greaterThan);
+        return equalsResult;
+    }
+} 
+
+module.exports.equals = equals;
+module.exports.lessThan = lessThan;
+module.exports.greaterThan = greaterThan;
+module.exports.arrayTraverser = arrayTraverser;
+module.exports.searchArrays = searchArrays;
---
+++ SearchFunctions.test.js
@@ -0,0 +1,41 @@
+const SearchFunctions = require('./SearchFunctions');
+const SearchResult = require('./SearchResultEnum');
+const ResultType = require('./ResultType');
+
+test('Equals exact', () => {
+    expect(SearchFunctions.equals(1,1)).toBe(SearchResult.FoundExact);
+});
+
+test('Equals not exact', () => {
+    expect(SearchFunctions.equals(1,2)).toBe(SearchResult.NotFound);
+});
+
+test('LessThan less', () => {
+    expect(SearchFunctions.lessThan(2,3)).toBe(SearchResult.FoundLess);
+});
+
+test('LessThan not less', () => {
+    expect(SearchFunctions.lessThan(3,2)).toBe(SearchResult.NotFound);
+});
+
+test('GreaterThan greater', () => {
+    expect(SearchFunctions.greaterThan(3,2)).toBe(SearchResult.FoundGreater);
+});
+
+test('LessThan not greater', () => {
+    expect(SearchFunctions.greaterThan(2,3)).toBe(SearchResult.NotFound);
+});
+
+test('ArrayTraverser left -> right and found', () => {
+    let arr = [1,2,3,4,5,6];
+    let result = SearchFunctions.arrayTraverser(5, arr, arr.length, false, SearchFunctions.equals);
+    expect(result).toEqual(new ResultType(SearchResult.FoundExact, 4, 5));
+});
+
+test('ArrayTraverser right -> left and not found', () => {
+    let arr = [6,5,4,3,2,1];
+    let result = SearchFunctions.arrayTraverser(7, arr, arr.length, true, SearchFunctions.equals);
+    expect(result).toEqual(new ResultType(SearchResult.NotFound, null, null));
+});
+
+test(')
